94228cf24e6a05ffe9991a6cfe1e14326fc7af09
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-28 22:00:35 -0400

    --made comments create

--- PAHStack/Controllers/PostController.cs
+++ PAHStack/Controllers/PostController.cs
@@ -35,7 +35,7 @@ namespace PAHStack.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            PostModel post = db.Posts.Find(Id);
+            PostModel post = db.Posts.Include(i => i.User).Where(w => w.Id == Id).First();
             if (post == null)
             {
                 return HttpNotFound();
@@ -44,8 +44,9 @@ namespace PAHStack.Controllers
         }
 
         // GET: Post/Create
-        public ActionResult Create()
+        public ActionResult Create(int? id)
         {
+
             return View();
         }
 
@@ -59,7 +60,7 @@ namespace PAHStack.Controllers
                 post.UserId = HttpContext.User.Identity.GetUserId();
                 db.Posts.Add(post);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", "Home");
             }
             return View(post);
         }
---
+++ PAHStack/Migrations/201704281845183_Mergedwithmaster1.cs
@@ -0,0 +1,16 @@
+namespace PAHStack.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Mergedwithmaster1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
